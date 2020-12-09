da38c1adf9f2bd14e217603953986583a31f3fe0
sharo-jef <cleared.for.approach.runway23@gmail.com>
2020-12-09 21:06:56 +0900

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+export default class ColorCode {
+    static black = '\u001b[30m';
+    static red = '\u001b[31m';
+    static green = '\u001b[32m';
+    static yellow = '\u001b[33m';
+    static blue = '\u001b[34m';
+    static magenta = '\u001b[35m';
+    static cyan = '\u001b[36m';
+    static white = '\u001b[37m';
+    static reset = '\u001b[0m';
+}
